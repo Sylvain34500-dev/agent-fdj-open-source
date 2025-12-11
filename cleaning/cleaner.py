3aec4b23bc1fa0c2ea231ebac4d647c042fd6380
Sylvain34500-dev <marchettosylvain83@hotmail.fr>
2025-12-11 14:10:59 +0100

    Update cleaner.py

--- cleaning/cleaner.py
+++ cleaning/cleaner.py
@@ -1,13 +1,3 @@
-from utils.logger import log
-
-
-def clean_data(data):
-    log("Nettoyage des données...")
-
-    cleaned = []
-    for m in data:
-        if not m["team1"] or not m["team2"]:
-            continue
-        cleaned.append(m)
-
-    return cleaned
+def clean(raw_data):
+    # Placeholder for cleaning system
+    return raw_data
