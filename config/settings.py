25e8ae6405003a919ebd1576c74f22e680be8411
Sylvain34500-dev <marchettosylvain83@hotmail.fr>
2025-12-11 16:37:18 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,2 +1,6 @@
+import os
+
 PRONOSOFT_URL = "https://www.pronosoft.com/fr/parions_sport/liste-parions-sport-plein-ecran.htm"
 
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
