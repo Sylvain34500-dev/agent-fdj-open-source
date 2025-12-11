e05a4b948191c02f7d8006755838e8611c683c3f
Sylvain34500-dev <marchettosylvain83@hotmail.fr>
2025-12-11 14:11:41 +0100

    Update predictor.py

--- predictions/predictor.py
+++ predictions/predictor.py
@@ -1,10 +1,2 @@
-from utils.logger import log
-
-
-def make_predictions(analysed_data):
-    log("Génération des prédictions...")
-
-    for m in analysed_data:
-        m["prediction"] = "1"  # Placeholder : victoire équipe 1
-
-    return analysed_data
+def make_prediction(clean_data):
+    return {"result": None}
