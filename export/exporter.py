0a70d7caa53417b06d09d4a47623fd7bd21b473c
Sylvain34500-dev <marchettosylvain83@hotmail.fr>
2025-12-11 14:11:20 +0100

    Update exporter.py

--- export/exporter.py
+++ export/exporter.py
@@ -1,11 +1,4 @@
-import json
 from utils.logger import log
 
-
-def export_results(predictions):
-    log("Export JSON...")
-
-    with open("predictions.json", "w", encoding="utf-8") as f:
-        json.dump(predictions, f, indent=2, ensure_ascii=False)
-
-    log("predictions.json généré.")
+def export_results(results):
+    log(f"Exporting results: {results}")
