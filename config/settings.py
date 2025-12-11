c828ab46f02b8a8436bd8420d8fa7d6b5b8fa98d
Sylvain34500-dev <marchettosylvain83@hotmail.fr>
2025-12-11 14:05:46 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,4 @@
 PRONOSOFT_URL = "https://www.pronosoft.com/fr/parions_sport/liste-parions-sport-plein-ecran.htm"
 
-TELEGRAM_BOT_TOKEN = "A_REMPLACER"
-TELEGRAM_CHAT_ID = "A_REMPLACER"
+TELEGRAM_BOT_TOKEN = 8501610794:AAENDq1GjMDnw41zMRcyA7_SvFzQ1GwKHRs
+TELEGRAM_CHAT_ID = 5312067776
